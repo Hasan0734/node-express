4e4f12e4823c6226d0f1c0f14fbad52192349c27
Hasan07 <Jahidhasan2856@gmail.com>
2021-06-20 21:14:04 +0600

    second time

--- index.js
+++ index.js
@@ -4,6 +4,19 @@ const rootCall = (req, res) => res.send("Thank you very much");
     
 
 app.get('/', (req, res) => {
-    res.send("hey your are not allowed")
+    const fruit = {
+        name: "Apple",
+        price: 36
+    }
+    res.send(fruit)
+})
+app.get('/frits/banana', (req, res) =>{
+    res.send({fruitt: 'banana', quantity: 1000, price: 10000})
+})
+const users = ["Asad", "Monir", "Rubby", "Jahid"]
+app.get('/users/:id', (req, res) => {
+   const userId=  req.params;
+   const name = users[userId];
+   res.send(name)
 })
 app.listen(3000, () => console.log("listening to port 3000"));
